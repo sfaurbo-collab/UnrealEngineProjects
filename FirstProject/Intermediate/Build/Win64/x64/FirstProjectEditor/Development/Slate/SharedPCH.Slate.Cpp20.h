f27ba39627d183cdfeb066bbce7475324b719c5d
sfaurbo-collab <sfaurbo@gmail.com>
2025-11-27 19:19:39 -0700

    Starting over

--- FirstProject/Intermediate/Build/Win64/x64/FirstProjectEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/UnrealEngineProjects/FirstProject/Intermediate/Build/Win64/x64/FirstProjectEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
